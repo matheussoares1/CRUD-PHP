88253cdb9d2122e750bc4935fef7f32ca54acfbf
Matheus Soares <111543203+matheussoares1@users.noreply.github.com>
2023-10-14 19:41:14 -0300

    Create banco.sql

---
+++ banco.sql
@@ -0,0 +1,29 @@
+--Criação da tabela usuarios
+CREATE TABLE usuarios(
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    nome_usuario VARCHAR(50) NOT NULL,
+    senha_usuario VARCHAR(50) NOT NULL
+);
+
+--Inserindo Dados
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('João', 'senha123');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Maria', 'senha456');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Pedro', 'senha789');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Ana', 'senhaabc');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Carlos', 'senhadef');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Luana', 'senha1234');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Rafael', 'senha5678');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Camila', 'senha9012');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Lucas', 'senhaxyz');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Isabel', 'senhalmn');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Gustavo', 'senhapqr');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Mariana', 'senha567');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Thiago', 'senha890');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Alice', 'senha000');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Bruno', 'senha999');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Laura', 'senhaabc');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Felipe', 'senha12345');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Julia', 'senhaxyz');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Diego', 'senha6789');
+INSERT INTO usuarios (nome_usuario, senha_usuario) VALUES ('Cristina', 'senha9876');
+
